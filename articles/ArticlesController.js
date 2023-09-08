38cd1c86ac42bb3e0e866422e902199d9242b98f
Natan Xavier <natanxavier@policiamilitar.sp.gov.br>
2023-09-08 17:52:17 -0300

    edicao-de-artigos
    Edição de artigos e mais.

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -57,4 +57,81 @@ router.post("/articles/delete", (req, res) => {
     }
 });
 
+router.get("/admin/articles/edit/:id", (req, res) => {
+    let id = req.params.id;
+
+    Article.findByPk(id).then(article => {
+        if (article.title != ""){
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit", { categories: categories, article: article });
+            })
+        } else {
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/");
+    });
+});
+
+router.post("/articles/update", (req, res) => {
+    let id = req.body.id;
+    let title = req.body.title;
+    let body = req.body.body;
+    let category = req.body.category;
+
+    Article.update({
+        title: title,
+        body: body,
+        categoryId : category,
+        slug: slugify(title)
+    }, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles");
+    }).catch(err => {
+        res.redirect("/");
+    });
+});
+
+//Paginação 
+router.get("/articles/page/:page", (req, res) => {
+    let page = req.params.page;
+    let offset = 0;
+    let amountElements = 4;
+
+    if (isNaN(page) || page == 1){
+        offset = 0;
+    } else {
+        offset = parseInt(page) * amountElements;
+    }
+
+    Article.findAndCountAll({
+        limit: amountElements,
+        offset: offset,
+        order: [
+            ['id', 'desc' ]
+        ]
+    }).then(articles => {
+        let next = true;
+
+        if (offset + amountElements >= articles.count){
+            next = false;
+        } else {
+            next = true;
+        }
+
+        let result = {
+            page: parseInt(page),
+            next: next,
+            articles: articles
+        }
+
+        Category.findAll().then(categories => {
+            res.render("admin/articles/page",{result: result, categories: categories});
+        });
+    });
+})
+
 module.exports = router;
